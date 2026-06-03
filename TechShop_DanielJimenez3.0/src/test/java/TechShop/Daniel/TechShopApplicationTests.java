7b1ab6e03e02bb89b295779062135c2a431f7860
DaniJim01 <219176878+DaniJim01@users.noreply.github.com>
2026-06-03 14:38:00 -0600

    Duplicacion 3.0

---
+++ TechShop_DanielJimenez3.0/src/test/java/TechShop/Daniel/TechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package TechShop.Daniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
