3e6390c942a3573c1a1b0d0637ce75cf902d819c
DaniJim01 <219176878+DaniJim01@users.noreply.github.com>
2026-07-10 15:29:57 -0600

    Semana 8
    Semana 8 funcional

--- TechShop_DanielJimenez4.0/src/main/java/TechShop/Daniel/repository/ProductoRepository.java
+++ TechShop_DanielJimenez4.0/src/main/java/TechShop/Daniel/repository/ProductoRepository.java
@@ -3,12 +3,19 @@ import TechShop.Daniel.domain.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-
-    // ← Reemplaza findByActivoTrue() por esto:
-    @Query("SELECT p FROM Producto p LEFT JOIN FETCH p.categoria WHERE p.activo = true")
     public List<Producto> findByActivoTrue();
+    
+    public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precionIf, double precioSup);
+    
+    @Query(nativeQuery = true,
+          value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")  
+    public List<Producto> consultaSQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
+
+    @Query(value = "SELECT p FROM Producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio  ASC")
+    public List<Producto> consultaJPQL(double precioInf, double precioSup);
 }
