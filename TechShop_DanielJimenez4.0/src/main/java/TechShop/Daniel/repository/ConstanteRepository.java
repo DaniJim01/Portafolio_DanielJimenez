7be1eb08c2f37ec32ded4e98448355dde75bf355
DaniJim01 <219176878+DaniJim01@users.noreply.github.com>
2026-08-12 11:42:36 -0600

    Semana 12 y 13

---
+++ TechShop_DanielJimenez4.0/src/main/java/TechShop/Daniel/repository/ConstanteRepository.java
@@ -0,0 +1,11 @@
+package TechShop.Daniel.repository;
+
+import TechShop.Daniel.domain.Constante;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConstanteRepository extends JpaRepository<Constante,Integer> {
+    
+    public Optional<Constante> findByAtributo(String atributo);
+    
+}
---
+++ TechShop_DanielJimenez4.0/src/main/java/TechShop/Daniel/repository/VentaRepository.java
@@ -0,0 +1,8 @@
+package TechShop.Daniel.repository;
+
+import TechShop.Daniel.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+    
+}
